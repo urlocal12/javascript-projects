d1c7ec2a2c5b1c1173c9514e08cf225a56844db8
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-11-05 20:58:22 -0800

    Update main.js

--- gradient/main.js
+++ gradient/main.js
@@ -4,13 +4,13 @@ function setup() {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.style('display', 'block');
   frameRate(30);
-  var sliderWidth = 80;
+  var sliderWidth = 200;
   r1 = createSlider(0, 255, 0);
   g1 = createSlider(0, 255, 0);
   b1 = createSlider(0, 255, 0);
-  r2 = createSlider(0, 255, 0);
-  g2 = createSlider(0, 255, 0);
-  b2 = createSlider(0, 255, 0);
+  r2 = createSlider(0, 255, 255);
+  g2 = createSlider(0, 255, 255);
+  b2 = createSlider(0, 255, 255);
   r1.position(10, 10);
   g1.position(10, 40);
   b1.position(10, 70);
@@ -28,13 +28,19 @@ function setup() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  r2.position(windowWidth - 10 - sliderWidth, 10);
+  g2.position(windowWidth - 10 - sliderWidth, 40);
+  b2.position(windowWidth - 10 - sliderWidth, 70);
 }
 
 function draw() {
   for (i = 0; i <= width; i++) {
-    var r = abs(r1.value() - r2.value()) / width * i + r1.value();
-    var g = abs(g1.value() - g2.value()) / width * i + g1.value();
-    var b = abs(b1.value() - b2.value()) / width * i + b1.value();
+    if (r1.value() < r2.value()) var r = abs(r1.value() - r2.value()) / width * i + r1.value();
+    else var r = (r1.value() - r2.value()) / width * i * -1 + r1.value();
+    if (g1.value() < g2.value()) var g = abs(g1.value() - g2.value()) / width * i + g1.value();
+    else var g = (g1.value() - g2.value()) / width * i * -1 + g1.value();
+    if (b1.value() < b2.value()) var b = abs(b1.value() - b2.value()) / width * i + b1.value();
+    else var b = (b1.value() - b2.value()) / width * i * -1 + b1.value();
     stroke(r, g, b);
     line(i, 0, i, height);
   }

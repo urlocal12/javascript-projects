d865a611672535e5d06c9cd7e88505b1145060c1
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-11-05 20:06:03 -0800

    Create main.js

---
+++ gradient/main.js
@@ -0,0 +1,61 @@
+var slidersHidden = false;
+
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas.style('display', 'block');
+  frameRate(30);
+  var sliderWidth = 80;
+  r1 = createSlider(0, 255, 0);
+  g1 = createSlider(0, 255, 0);
+  b1 = createSlider(0, 255, 0);
+  r2 = createSlider(0, 255, 0);
+  g2 = createSlider(0, 255, 0);
+  b2 = createSlider(0, 255, 0);
+  r1.position(10, 10);
+  g1.position(10, 40);
+  b1.position(10, 70);
+  r2.position(width - 10 - sliderWidth, 10);
+  g2.position(width - 10 - sliderWidth, 40);
+  b2.position(width - 10 - sliderWidth, 70);
+  r1.style("width", sliderWidth + "px");
+  g1.style("width", sliderWidth + "px");
+  b1.style("width", sliderWidth + "px");
+  r2.style("width", sliderWidth + "px");
+  g2.style("width", sliderWidth + "px");
+  b2.style("width", sliderWidth + "px");
+}
+
+function draw() {
+  for (i = 0; i <= width; i++) {
+    if (r1.value() < r2.value()) var r = (r1.value() - r2.value()) / width * i + r1.value();
+    else var r = (r1.value() - r2.value()) / width * i + r2.value();
+    if (g1.value() < g2.value()) var g = (g1.value() - g2.value()) / width * i + g1.value();
+    else var g = (g1.value() - g2.value()) / width * i + g2.value();
+    if (b1.value() < b2.value()) var b = (b1.value() - b2.value()) / width * i + b1.value();
+    else var b = (b1.value() - b2.value()) / width * i + b2.value();
+    stroke(r, g, b);
+    line(i, 0, i, height);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (slidersHidden == true) {
+      r1.show();
+      g1.show();
+      b1.show();
+      r2.show();
+      g2.show();
+      b2.show();
+      slidersHidden = false;
+    } else {
+      r1.hide();
+      g1.hide();
+      b1.hide();
+      r2.hide();
+      g2.hide();
+      b2.hide();
+      slidersHidden = true;
+    }
+  }
+}

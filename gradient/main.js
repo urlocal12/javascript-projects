8807b42d16e43a293a2fd6e3d8150c3309120155
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-11-06 19:49:48 -0800

    Update main.js

--- gradient/main.js
+++ gradient/main.js
@@ -1,9 +1,13 @@
 var sliders = [];
 var c = [];
+var middleToggler;
+var mTogSize = 200;
+var twoColors = false;
 var slidersHidden = false;
 var hintY;
 var sliderWidth = 255;
 var textY = 160;
+var middleTextY = 160;
 
 function load(r1, g1, b1, r2, g2, b2, startX, endX) {
   for (i = 0; i <= endX - startX; i++) {
@@ -18,9 +22,28 @@ function load(r1, g1, b1, r2, g2, b2, startX, endX) {
   }
 }
 
+function toggleNumOfColors() {
+  if (twoColors) {
+    twoColors = false;
+    for (i = 3; i < 6; i++) sliders[i].show();
+    middleTextY = textY;
+  }
+  else {
+    twoColors = true;
+    for (i = 3; i < 6; i++) sliders[i].hide();
+    middleTextY = 0 - height;
+  }
+}
+
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   frameRate(30);
+  middleToggler = createButton("Toggle Middle Color");
+  middleToggler.position(width / 2 - mTogSize / 2, 200);
+  middleToggler.size(mTogSize);
+  middleToggler.style("border", "none");
+  middleToggler.style("font-size", "20px");
+  middleToggler.mousePressed(toggleNumOfColors);
   hintY = height / 2;
   for (i = 0; i < 9; i++) sliders.push(createSlider(0, 255, random(0, 255)));
   sliders[0].position(100, 50);
@@ -43,19 +66,24 @@ function windowResized() {
   sliders[6].position(width - 100 - sliderWidth, 50);
   sliders[7].position(width - 100 - sliderWidth, 80);
   sliders[8].position(width - 100 - sliderWidth, 110);
+  middleToggler.position(width / 2 - mTogSize / 2, 200);
 }
 
 function draw() {
   for (i = 0; i < 9; i++) c[i] = sliders[i].value();
-  load(c[0], c[1], c[2], c[3], c[4], c[5], 0, width / 2);
-  load(c[3], c[4], c[5], c[6], c[7], c[8], width / 2 + 1, width);
+  if (twoColors) {
+    load(c[0], c[1], c[2], c[6], c[7], c[8], 0, width);
+  } else {
+    load(c[0], c[1], c[2], c[3], c[4], c[5], 0, width / 2);
+    load(c[3], c[4], c[5], c[6], c[7], c[8], width / 2 + 1, width);
+  }
   textSize(32);
   stroke("black");
   textAlign(CENTER, CENTER);
   text("Press SPACE to toggle settings visibility", width / 2, hintY);
   textSize(24);
   text("R: " + c[0] + " G: " + c[1] + " B: " + c[2], 100 + sliderWidth / 2, textY);
-  text("R: " + c[3] + " G: " + c[4] + " B: " + c[5], width / 2, textY);
+  text("R: " + c[3] + " G: " + c[4] + " B: " + c[5], width / 2, middleTextY);
   text("R: " + c[6] + " G: " + c[7] + " B: " + c[8], width - 100 - sliderWidth / 2, textY);
 }
 
@@ -63,13 +91,20 @@ function keyPressed() {
   if (keyCode === 32) {
     hintY -= height;
     if (slidersHidden == true) {
-      for (i = 0; i < 9; i++) sliders[i].show();
+      for (i = 0; i < 9; i++) {
+        if (i >= 3 && i <= 6 && twoColors) continue;
+        sliders[i].show();
+      }
+      middleToggler.show();
       slidersHidden = false;
       textY = 160;
+      if (!twoColors) middleTextY = 160;
     } else {
       for (i = 0; i < 9; i++) sliders[i].hide();
+      middleToggler.hide();
       slidersHidden = true;
       textY = -100;
+      middleTextY = -100;
     }
   }
 }
